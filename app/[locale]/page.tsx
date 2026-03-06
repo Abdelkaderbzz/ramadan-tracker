fbbafad16b0739a524769bcc740e48d39493e832
hosssabry568-ui <hosssabry568@gmail.com>
2026-03-06 16:42:15 +0200

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import RamadanTracker from '@/components/layout/ramadan-tracker';
 
 export default function Home() {
   return (
-    <main className='min-h-screen bg-gradient-to-b from-purple-50 to-white dark:from-gray-950 dark:to-gray-900'>
+<main className='min-h-screen bg-gradient-to-b from-purple-50 to-white dark:from-gray-950 dark:to-gray-900'>
       <RamadanTracker />
     </main>
   );
